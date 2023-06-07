0227b7e4a505be8b6fa01b28631aea56aea389cc
Diego-CP <pinedo.dc@gmail.com>
2023-06-07 16:43:56 -0600

    Changed test file to recognize app

--- A01781631/tests/test_app.py
+++ A01781631/tests/test_app.py
@@ -1,9 +1,9 @@
-from app import lambda_handler
+from src import app
 import pytest
 import sys
 sys.path.append('../src')
 
 
 def test_lambda_handler():
-    result = lambda_handler(None , None)
+    result = app.lambda_handler(None , None)
     assert result == "Hola, A01781631"
